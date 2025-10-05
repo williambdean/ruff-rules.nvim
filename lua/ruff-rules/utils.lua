2d045282877fdd9deb6066c9875a65b83f6a3e29
Will Dean <wd60622@gmail.com>
2025-10-04 23:29:13 -0400

    add support for rule lookup

--- lua/ruff-rules/utils.lua
+++ lua/ruff-rules/utils.lua
@@ -4,13 +4,12 @@ local M = {}
 
 --- Split ruff rule into group and number
 M.split_rule = function(rule)
-  local letters = rule:match("^[A-Z]+")
-  local numbers = rule:match("%d+$")
+  local letters = rule:match "^[A-Z]+"
+  local numbers = rule:match "%d+$"
   return {
     code = letters,
     number = numbers,
   }
 end
-
 
 return M
